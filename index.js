6a435b218631c3f31556526adb35b21164452241
NickR <nikitos.aka.zed@gmail.com>
2019-04-23 13:53:19 +0300

    add db connection

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const pool = require('./config/config')
 
 app.route('/').get((request, response) => {
     response.send('Hello world')
+})
+
+app.route('/api/t/:group').get((request, response) => {
+    let ngroup = request.params['group']
+    response.send(`Hello ${ngroup}`)
+})
+
+app.route('/api/students').get((request, response) => {
+    pool.query('Select * from students', (err, result, fields) => {
+        if (err) throw err
+        response.send(result);
+    })
+})
+
+app.route('/api/students/:n_z').get((request, response) => {
+    const n_z = request.params['n_z']
+    pool.query('Select * from students where n_z = ?', [n_z],
+    (err, result, fields) => {
+        if (err) throw err
+        response.send(result);
+    })
 })
 
 app.listen(8080, () => {
